a4590d954a221abb6b52564767771d0bc02c39cf
unknown <soojlee0106@naver.com>
2023-01-10 12:24:51 +0900

    Working on debugging

--- frontend/react_app/src/Urls.js
+++ frontend/react_app/src/Urls.js
@@ -1,8 +1,16 @@
 
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
+
+export const ProtectedRoute = ({ isAuthenticated, children, ...rest }) => {
+    if (!isAuthenticated) {
+        // user is not authenticated
+        return <Navigate to="/login" />;
+    };
+};
+
 
 function Urls(props) {
     return (
@@ -10,7 +18,7 @@ function Urls(props) {
             <BrowserRouter>
                 <Routes >
                     <Route path="/login/" element={<Login {...props} />} />
-                    <Route path="/" element={<Home {...props} />} />
+                    <Route exact path="/" element={<ProtectedRoute><Home {...props} /></ProtectedRoute>} />
                 </Routes >
             </BrowserRouter>
         </div>
